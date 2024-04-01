6ce2bd85e13a86ec26e7408bfc5ac6ea8743d4cc
펜타곤 <seonwoo0808@pentagons-MacBook-Pro.local>
2024-04-01 21:59:09 +0900

    🧪 Logging library teset

--- controllers/reportctrl/send.go
+++ controllers/reportctrl/send.go
@@ -1,8 +1,9 @@
 package reportctrl
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/log"
 	"github.com/kamva/mgm/v3"
 	"pentag.kr/dimimonster/middleware"
 	"pentag.kr/dimimonster/models"
@@ -37,13 +38,13 @@ func SendReportCtrl(c *fiber.Ctx) error {
 		body.Reason,
 	)
 	if err != nil {
-		log.Error(err)	
+		log.Println(err)
 		return c.Status(500).SendString("Internal Server Error")
 	}
 
 	err = mgm.Coll(newReport).Create(newReport)
 	if err != nil {
-		log.Error(err)
+		log.Println(err)
 		return c.Status(500).SendString("Internal Server Error")
 	}
 
