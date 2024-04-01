4a0745cecdf5c27874c034d6b48e775dc652a0fc
펜타곤 <seonwoo0808@pentagons-MacBook-Pro.local>
2024-04-01 23:06:04 +0900

    ✨ Add date on discord embed and report process

--- utils/discord/webhook.go
+++ utils/discord/webhook.go
@@ -13,13 +13,17 @@ var username = "신고봇"
 var color = "16744272"
 
 func SendReportWebhook(report *models.Report) {
-	links := "[이미지 보기](https://dimi.monster/?id=&&id&&) [이미지 삭제 처리](https://dimi.monster/admin/delete/&&id&&?key=&&key&&)"
+	links := "[이미지 보기](https://dimi.monster/?id=&&id&&) | [이미지 삭제 처리](https://dimi.monster/admin/delete/&&id&&?key=&&key&&) | [이미지 삭제 및 차단 처리](https://dimi.monster/admin/deleteban/&&id&&?key=&&key&&) | [신고 철회 처리](https://dimi.monster/admin/withdraw/&&id&&?key=&&key&&) | [신고자 차단 처리](https://dimi.monster/admin/banreporter/&&id&&?key=&&key&&)"
 	links = strings.ReplaceAll(links, "&&id&&", report.TargetImageID)
 	links = strings.ReplaceAll(links, "&&key&&", report.Secret)
+	date := report.CreatedAt.Format("2006-01-02 15:04:05")
 	embed := discordwebhook.Embed{
 		Title: &report.Category,
 		Description: &report.Reason,
 		Color: &color,
+		Footer: &discordwebhook.Footer{
+			Text: &date,
+		},
 	}
 
 	message := discordwebhook.Message{
